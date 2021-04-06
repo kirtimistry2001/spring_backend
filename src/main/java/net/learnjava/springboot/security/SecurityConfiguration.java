d87fdd8925a2cbf24bfa27a2a49761bbb963d26d
kirtimistry2001 <kirtimistry2001@yahoo.com>
2021-04-05 21:56:04 -0400

    setup Authentication and Authorization for
    'USER' and 'ADMIN' Role

--- src/main/java/net/learnjava/springboot/security/SecurityConfiguration.java
+++ src/main/java/net/learnjava/springboot/security/SecurityConfiguration.java
@@ -1,15 +1,31 @@
 package net.learnjava.springboot.security;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
+	/**
+	 * configure Authentication using Authentication Manager
+	 */
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(auth);
+		
+	}
+	
+	/**
+	 * set up Authorization using HttpSecurity
+	 */
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()  //use when need to authorize all endspoint
+		.antMatchers("/admin").hasRole("ADMIN") ///admin endpoint for loggedin user with role  'ADMIN'
+		.antMatchers("/user").hasAnyRole("ADMIN","USER") ///users endpoing for logged in user with 'User" role, User can have ADMIN role as well. so its any Role
+		.antMatchers("/*").permitAll() // for all end points it needs to access login form. Note:never put this first
+		.and().formLogin();
+
 	}
 }
