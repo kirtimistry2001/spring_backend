38c13a5c701c7f3b2d71d0f37ccc8fa8d4eb4572
kirtimistry2001 <kirtimistry2001@yahoo.com>
2021-04-05 22:49:42 -0400

    Give some temporary constant hardcoded  values

--- src/main/java/net/learnjava/springboot/service/MyUserDetails.java
+++ src/main/java/net/learnjava/springboot/service/MyUserDetails.java
@@ -1,52 +1,62 @@
 package net.learnjava.springboot.service;
 
+import java.util.Arrays;
 import java.util.Collection;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public class MyUserDetails implements  UserDetails{
 
+
+	private static final long serialVersionUID = 1L;
+	private String userName;
+
+	MyUserDetails() {
+		//empty constructor
+	}
+	MyUserDetails(String userName) {
+		this.userName = userName;
+	}
+	
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
-		return null;
+		// temporary passing some constant values
+		return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
 	}
 
 	@Override
 	public String getPassword() {
-		// TODO Auto-generated method stub
-		return null;
+		return "pass"; //temporary hardcoded
 	}
 
 	@Override
 	public String getUsername() {
-		// TODO Auto-generated method stub
-		return null;
+		return userName;
 	}
 
 	@Override
 	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public boolean isAccountNonLocked() {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public boolean isCredentialsNonExpired() {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public boolean isEnabled() {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 }
