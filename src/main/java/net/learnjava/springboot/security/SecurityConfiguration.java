28afffd6f565c136fc54f6d6e5d3df5a9aea17f7
kirtimistry2001 <kirtimistry2001@yahoo.com>
2021-04-05 21:50:29 -0400

    Added Spring security for user authentication

---
+++ src/main/java/net/learnjava/springboot/security/SecurityConfiguration.java
@@ -0,0 +1,15 @@
+package net.learnjava.springboot.security;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// TODO Auto-generated method stub
+		super.configure(auth);
+	}
+}
